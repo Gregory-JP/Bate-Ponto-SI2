4418e98618f986c6a1d1632582d9faf48e032e9f
thelopesbr <jefersonlopes.sjn@hotmail.com>
2022-06-26 21:37:40 -0300

    comentando o middleware que eu esqueci

--- src/js/middleware/permisson.js
+++ src/js/middleware/permisson.js
@@ -1,5 +1,6 @@
-
+//esse é um middleware que verifica se existe uma sessão no sessionStorage
 if(sessionStorage.length < 2){
+    //se não tiver ele redireciona o usuario para a tela de login, impedindo o de acessar
     window.location.assign("/src/view/login.html");
 }
 
