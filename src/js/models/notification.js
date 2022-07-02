3c84e42a1c3041573de8ca1116f3ee60dfae5c17
thelopesbr <jefersonlopes.sjn@hotmail.com>
2022-07-01 23:24:02 -0300

    documentando o restante

--- src/js/models/notification.js
+++ src/js/models/notification.js
@@ -1,15 +1,24 @@
+//criando uma classe de notificação, sendo necessarios uma mensagem, uma flags indicando se é mensagem de sucesso
+// ou se a mensagem é de erro, e por ultimo um direcionamento.
+//o campo de sucess e redirect são opcionais
 class Notification{
     constructor(msg,sucess,redirect){
         this.sucess = sucess;
         this.msg = msg;
         this.redirect = redirect;
     }
+    //ativa a notificação
     activate(){
+        //coleta os elementos html
         const div = document.getElementById('notification');
         const message = document.getElementById('message_notification');
+        //define o icone
         let icon = this.sucess ? '✔️' : '❌'
+        //mostra a notificação na tela
         div.style.display='block';
         message.innerHTML = icon + this.msg;
+        //seta um tempo para tirar a notificação da tela e depois redireciona
+        //se for o caso
         setTimeout(() => {
             div.style.display='none';
             if(this.redirect){
@@ -18,5 +27,5 @@ class Notification{
         }, 2000);
     }
 }
-
+//exporta o modulo
 export default Notification;
--- src/js/models/register.js
+++ src/js/models/register.js
@@ -1,32 +1,44 @@
+//cria um modelo para controlar os registros dos pontos
+//no contrutor nos verificamos se o user é admin ou nao
+//pegamos a tabela na qual iremos add os registro em tela
+// e criamos uma variavel de apoio para os registros
 class Register{
     constructor(table,admin=false){
         this.admin = admin;
         this.table = table;
         this._registers = [];
-        this._dates = [];
     }
+    //coleta os registros
     _get_registers(){
+        //se não for admin, entao coletará somente os registros do user logado
         if(!this.admin){
             let email;
+            //pega o email da session, ele já diferencia a sessao da extenção
             for(let i=0; i < 2; i++){
                 if(sessionStorage.key(i) != 'IsThisFirstTime_Log_From_LiveServer'){
                     email = sessionStorage.key(i)
                 }
             }
+            //coleta os registros que estão no localStorage
             for(let i=0; i < localStorage.length; i++){
+                //pega somente os registros que nao sao de user
                 let point = !localStorage.key(i).includes('@');
                 if(point){
+                    //pega o item no localStorage
                     let key = localStorage.key(i)
                     let register = localStorage.getItem(key);
+                    //coloca no formato json
                     register = JSON.parse(register);
+                    //verifica se o user no registro é o user da sessão
                     if(register._user == email){
-                        console.log(email)
-                        console.log(register._user)
+                        //coloca na variavel de apoio
                         this._registers.push(register);
                     }
                 }
             }
         }
+        //se for admin, coleta todos os registros repetindo os passos da linha 23
+        // removendo a verificação de user da linha 33
         else{
             for(let i=0; i < localStorage.length; i++){
                 let point = !localStorage.key(i).includes('@');
@@ -39,26 +51,30 @@ class Register{
             }
         }
     }
+    //atualiza na tela as informações coletadas
     save(){
+        //chama a função que coleta os registros
         this._get_registers();
+        //para cada registro cria uma linha na tabela 
         this._registers.forEach((register) => {
-  
+            //cria uma linha
             let tr = document.createElement('tr');
+            //cria os cabeçalhos
             let user = document.createElement('th');
             let date = document.createElement('th');
             let time = document.createElement('th');
-
+            //atualiza os cabeçalhos
             user.innerHTML = register._user;
             date.innerHTML = register._date;
             time.innerHTML = register._time;
-
+            //add os cabeçalhos na linha
             tr.appendChild(user);
             tr.appendChild(date);
             tr.appendChild(time);
-
+            //add a linha na coluna
             table.appendChild(tr)
         }) 
     }
 }
-
+//exporta o modulo
 export default Register;
